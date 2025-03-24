02713fa1e50e7bc309cf11792e3c32f9a27794b3
Samuel Messmer <samuel.messmer@web.de>
2025-03-24 04:42:21 +0100

    same

--- src/edu/kit/kastel/recommendationsystem/view/commands/CommandLoad.java
+++ src/edu/kit/kastel/recommendationsystem/view/commands/CommandLoad.java
@@ -3,7 +3,6 @@ package edu.kit.kastel.recommendationsystem.view.commands;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.kit.kastel.recommendationsystem.view.Result;
