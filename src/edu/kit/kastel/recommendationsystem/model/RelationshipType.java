69ba000dc2d25e3452430627e1055321e3a2cc4f
Samuel Messmer <samuel.messmer@web.de>
2025-03-24 03:11:21 +0100

    fix: isAllowedBetween

--- src/edu/kit/kastel/recommendationsystem/model/RelationshipType.java
+++ src/edu/kit/kastel/recommendationsystem/model/RelationshipType.java
@@ -98,11 +98,8 @@ public enum RelationshipType {
      * @return {@code true} if the relationship is allowed, {@code false} otherwise
      */
     public static boolean isAllowedBetween(RelationshipDTO relationship) {
-        if (relationship.subject().isOfType(NodeType.CATEGORY) && relationship.object().isOfType(NodeType.CATEGORY)) {
+        if (relationship.subject().isOfType(NodeType.CATEGORY) || relationship.object().isOfType(NodeType.CATEGORY)) {
             return CATEGORY_ALLOWED_RELATIONSHIPS.contains(relationship.predicate());
-        }
-        if (relationship.subject().isOfType(NodeType.PRODUCT) && relationship.object().isOfType(NodeType.PRODUCT)) {
-            return !CATEGORY_ALLOWED_RELATIONSHIPS.contains(relationship.predicate());
         }
         return true;
     }
