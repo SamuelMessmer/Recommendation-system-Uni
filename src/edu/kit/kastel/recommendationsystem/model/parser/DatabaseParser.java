55d4766998685ad073ba6be920257d010cdaff39
Samuel Messmer <samuel.messmer@web.de>
2025-03-24 05:04:45 +0100

    same

--- src/edu/kit/kastel/recommendationsystem/model/parser/DatabaseParser.java
+++ src/edu/kit/kastel/recommendationsystem/model/parser/DatabaseParser.java
@@ -121,13 +121,13 @@ public final class DatabaseParser {
                     throw new DataParsException(null, "detacted several products with the same id.");
                 }
                 if (relationship.subject() instanceof Product && existingNode instanceof Product
-                        && ((Product) relationship.subject()).getId() == ((Product) existingNode).getId()
+                        && ((Product) relationship.subject()).getId() != ((Product) existingNode).getId()
                         && relationship.subject().getName() == existingNode.getName()
                         && !relationship.subject().equals(existingNode)) {
                     throw new DataParsException(null, "detacted several products with the same id.");
                 }
                 if (relationship.object() instanceof Product && existingNode instanceof Product
-                        && ((Product) relationship.object()).getId() == ((Product) existingNode).getId()
+                        && ((Product) relationship.object()).getId() != ((Product) existingNode).getId()
                         && relationship.object().getName() == existingNode.getName()
                         && !relationship.object().equals(existingNode)) {
                     throw new DataParsException(null, "detacted several products with the same id.");
